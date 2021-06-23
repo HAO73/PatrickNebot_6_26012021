2266c0cea57dba0aaa6ecfb9a28e6dd3f12c4fd7
HAO73 <57308635+HAO73@users.noreply.github.com>
2021-06-23 18:52:30 +0000

    Commit 3

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,7 +9,12 @@ const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
-mongoose.connect('mongodb+srv://hao:shangai2025@cluster0.qs7vs.mongodb.net/SoPekocko?retryWrites=true&w=majority',
+require('dotenv').config();
+
+const atlasUser = process.env.ATLAS_USER
+const atlasPassword = process.env.ATLAS_PASSWORD
+
+mongoose.connect(`mongodb+srv://${atlasUser}:${atlasPassword}@cluster0.qs7vs.mongodb.net/SoPekocko?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
